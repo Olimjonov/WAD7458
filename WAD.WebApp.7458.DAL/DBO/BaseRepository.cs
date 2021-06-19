ccfe154ceedde5cabb97a7c03a27e4dbb05ce873
Olimjonov <dolimjonov@inbox.ru>
2021-06-19 17:27:45 +0500

    BaseRepository class created

---
+++ WAD.WebApp.7458.DAL/DBO/BaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WAD.WebApp._7458.DAL.DBO
+{
+    public class BaseRepository
+    {
+        protected readonly BikeDbContext _dbContext;
+
+        protected BaseRepository(BikeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
